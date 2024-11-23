36d1d13363c3c4f1e7b914c25243735c865e0986
dezzywezzy1 <jdez@JV-PC>
2024-11-22 22:52:37 -0600

    implemented Data() class to handle API requests and local data storage
    the local data storage solely serves the purpose of not having to continuously call the API. Next steps are to implement some sort of initial transform method to Data() and to repurpose the save() method to upload all instances to GCS.

---
+++ cfb_data.py
@@ -0,0 +1,126 @@
+import os
+import pandas as pd
+import requests
+from dotenv import find_dotenv, load_dotenv
+import time
+from pathlib import Path
+
+
+
+
+
+
+class Data:
+    api_url = "https://api.collegefootballdata.com/"
+    instances = []
+    
+    def __init__(self):
+        self.data_frame = None
+        self.json_data = None
+        self.csv_path = None
+        Data.instances.append(self)
+        path = find_dotenv('.env')
+        load_dotenv(path)
+        self.api_key= os.environ.get("CFB_API_KEY")
+        self.headers = {
+            'Authorization': "Bearer " +  self.api_key,
+            'Accept': 'application/json'
+        }
+     
+        
+    @classmethod
+    def get_instances(cls):
+        return cls.instances
+    
+    
+    @staticmethod
+    def is_flat(data):
+        if isinstance(data, list):
+            if isinstance(data[0], dict):
+                if all(not isinstance(value, (dict, list, tuple, set)) for value in data[0].values()):
+                    return True
+        elif isinstance(data, dict):
+            if all(not isinstance(value, (dict, list, tuple, set)) for value in data.values()):
+                    return True
+        return False  
+    
+    @staticmethod
+    def timing_decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f"API call {list(kwargs.items())[0]} executed in {end_time - start_time:.4f} seconds")
+            return result
+        return wrapper  
+    
+    
+        
+    def save(self, file_path: str | Path):
+        if not file_path:
+            file_path = self.csv_path
+            if not self.csv_path:
+                raise AttributeError("No .csv file path found. Enter file_path argument to save.")
+        file_path = Path(file_path)
+        
+        if self.data_frame is None:
+            raise ValueError("No data to save. Make sure self.data_frame is populated.")
+        
+        try:
+            self.data_frame.to_csv(file_path)
+            self.csv_path = file_path
+        except Exception as e:
+            raise IOError(f"Error saving DataFrame to CSV: {e}")
+    
+            
+    
+    @timing_decorator
+    def request(self, endpoint: str, params: dict = None) -> any:
+        if not endpoint:
+            raise TypeError("request() requires 'endpoint' argument")
+        if not isinstance(endpoint, str):
+            raise TypeError("'endpoint' argument must be of type: str")
+        if params:
+            if not isinstance(params, dict):
+                raise TypeError("'params' argument must be of type: dict")
+       
+        if params:
+            try:
+                response = requests.get(f"{self.api_url}{endpoint}", params=params, headers=self.headers)
+            except requests.RequestException as e:
+                raise RuntimeError(f"Error during API request: {e}")
+        else:
+            try:
+                response = requests.get(f"{self.api_url}{endpoint}", headers=self.headers)
+            except requests.RequestException as e:
+                raise RuntimeError(f"Error during API request: {e}")   
+            
+        self.json_data = response.json()  
+         
+        if self.is_flat(self.json_data):
+            self.data_frame = pd.DataFrame(self.json_data)
+            return self.data_frame
+        elif isinstance(self.json_data, list):
+            row = (self.json_data)[0]
+            problem_data = {key: row[key] for key in row.keys() if isinstance(row[key], (dict, list, tuple, set))}
+            print(f"API Request Success, but not able to convert into DataFrame. Data is not flat. \n\n {problem_data}")
+        elif isinstance(self.json_data, dict):
+            problem_data = {key: (self.json_data)[key] for key in self.json_data.keys() if isinstance((self.json_data)[key], (dict, list, tuple, set))}
+            print(f"API Request Success, but not able to convert into DataFrame. Data is not flat. \n\n {problem_data}")
+        
+        
+        return self.json_data
+    
+    
+    def load(self, file_path: str | Path) -> pd.DataFrame:
+        file_path = Path(file_path)
+        if file_path.suffix != ".csv":
+            raise ValueError(f"{file_path} must reference a CSV file")
+        if file_path.exists():
+            try:
+                self.data_frame = pd.read_csv(file_path)
+                return pd.read_csv(file_path)
+            except Exception as e:
+                raise NotImplementedError(f"Error reading CSV file {file_path}: {e}")
+        else:
+            raise FileNotFoundError(f"{file_path} does not exist")
--- ingest_data.py
+++ ingest_data.py
@@ -1,41 +1,29 @@
-import os
-import pandas as pd
-import requests
-import json
+from cfb_data import Data
 from datetime import datetime
-from dotenv import load_dotenv
-import dotenv
-import time
-from pathlib import Path
 
-
-path = dotenv.find_dotenv('.env')
-load_dotenv(path)
-api_key= os.environ.get("CFB_API_KEY")
 
 #get current year of CFB. Consider the start of a new season June 1.
 if datetime.now().month <= 6:
     current_football_year = (datetime.now().year) - 1
 else:
     current_football_year = datetime.now().year
+         
 
 
-def timing_decorator(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"API call {list(kwargs.items())[0]} executed in {end_time - start_time:.4f} seconds")
-        return result
-    return wrapper  
-    
-
+def main():
+    player_stats = Data()
+    player_stats.request('stats/player/season', params={
+        'year': current_football_year
+    })
+    player_stats.save(f'player_stats_{current_football_year}.csv')
+    print(player_stats.data_frame.head(15))
 # test that original data has the same amount of keys/columns as the flattened data
-def test_cols(data: dict, df: pd.DataFrame) -> bool:
+''' DON'T NEED THIS QUITE YET'''
+'''def test_cols(data: dict, df: pd.DataFrame) -> bool:
     count_nested_keys = 0
     for key in data.keys():
-        if type(data[key]) == dict:
-            count_nested_keys += len(data[key].keys())
+        if isinstance(data[key], (list, dict, tuple)): 
+            count_nested_keys += len(data[key])
             # don't double count original key
             count_nested_keys -= 1
     count_keys = len(data.keys())
@@ -43,17 +31,16 @@ def test_cols(data: dict, df: pd.DataFrame) -> bool:
     if count_keys == len(df.columns):
         return True
     else:
-        return False
+        return False'''
 
 
-def test_rows(data: list, df: pd.DataFrame) -> bool:
-    if len(data) == len(df):
-        return True
-    else:
-        return False
-    
 
 
+
+
+''' SAVING BELOW FOR LATER SO I DON'T HAVE TO RE-TYPE THE ENDPOINTS'''
+
+'''
 # get only FBS conference names and abbreviations for API parameters
 conferences= get_cfb_data(endpoint="/conferences")
 fbs_conference_names = [conf['name'] for conf in conferences if 'fbs' in conf['classification']]
@@ -72,8 +59,8 @@ df_week_dates = pd.DataFrame(week_dates)
 
 #get current year game results for all fbs schools
 #fix nested structure of 'line_score'
-'''game_results = get_cfb_data('/games', params={'year': current_football_year, 'division': 'fbs'})
-df_game_results = pd.DataFrame(game_results)'''
+game_results = get_cfb_data('/games', params={'year': current_football_year, 'division': 'fbs'})
+df_game_results = pd.DataFrame(game_results)
 
 #get all current year game media information
 media_info = get_cfb_data(endpoint='/games/media', params={'year': current_football_year, 'division': 'fbs'})
@@ -94,83 +81,8 @@ df_team_rankings = pd.json_normalize(
     meta=["season", "seasonType", "week", ["polls", "poll"]],
     meta_prefix=''
 )
-
-print(f"\n\n\n{df_team_rankings.head(15)}\n\n\n")
-df_team_rankings.info()
-
-
-'''
--Don't worry about looping through FBS conferences as this significantly slows down the script-
--Start saving the data that doesn't have any issues as files so we can stop calling the API-
 '''
 
-class Cfb_Data():
-    
-    @timing_decorator
-    def request(self, endpoint: str, api_key: str, params: dict = None) -> any:
-        if not endpoint:
-            raise TypeError("request() requires 'endpoint' argument")
-        if not isinstance(endpoint, str):
-            raise TypeError("'endpoint' argument must be of type: str")
-        if params:
-            if not isinstance(params, dict):
-                raise TypeError("'params' argument must be of type: dict")
-        
-        headers = {
-            'Authorization': "Bearer " +  api_key,
-            'Accept': 'application/json'
-        }
-        if params:
-            try:
-                response = requests.get(f"https://api.collegefootballdata.com{endpoint}", params=params, headers=headers)
-            except Exception as e:
-                return str(e)
-        else:
-            try:
-                response = requests.get(f"https://api.collegefootballdata.com{endpoint}", headers=headers)
-            except Exception as e:
-                return str(e)
 
-        return response.json()
-    
-    def load(self, file_name: str | Path) -> pd.DataFrame:
-        try:
-            file_name = Path(file_name)
-        except Exception as e:
-            return str(e)
-        
-        if file_name.exists():
-            try:
-                return pd.read_csv(file_name)
-            except Exception as e:
-                return str(e)
-        else:
-            raise FileNotFoundError("Could not find file at given path")
-    
-    def save(self, data: pd.DataFrame, file_path: str | Path):
-        try:
-            file_name = Path(file_name)
-        except Exception as e:
-            return str(e)
-        
-        if isinstance(data, pd.DataFrame) or (isinstance(data, list) and all(isinstance(item, dict) for item in data)):
-            try:
-                if isinstance(data, pd.DataFrame):
-                    data.to_csv(file_path)
-                else:
-                    pd.DataFrame(data).to_csv(file_path)
-            except Exception as e:
-                return str(e)
-            
-        else:
-            raise TypeError("data must be of type DataFrame or flattened JSON")
-    
-    def flatten_json():
-        pass
-        '''
-        -create function to flatten json data    
-        '''
-    
-    '''
-    -input data integrity tests from top of page into request()
-    '''
+if __name__ == "__main__":
+    main()
