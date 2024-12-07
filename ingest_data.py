c4e35791c2d22401d4cc367d00937755864f1a36
dezzywezzy1 <jdez@JV-PC>
2024-12-06 21:54:10 -0600

    restructured data ingestion. Flatten data next before pushing to GCS.

--- ingest_data.py
+++ ingest_data.py
@@ -1,5 +1,8 @@
 from cfb_data import Data
 from datetime import datetime
+import json
+import csv
+import sys
 
 
 #get current year of CFB. Consider the start of a new season June 1.
@@ -8,11 +11,85 @@ if datetime.now().month <= 6:
 else:
     current_football_year = datetime.now().year
          
+    data_sources= {
+        'fbs_teams': {
+            'endpoint': 'teams/fbs',
+            'params': {
+                'year': current_football_year
+            }
+        },
+        'records': {
+            'endpoint': 'records',
+            'params': {
+                'year': current_football_year
+            }
+        },
+        'game_results': {
+            'endpoint': 'games',
+            'params': {
+                'year': current_football_year,
+                'division': 'fbs'
+            }
+        },
+        'week_dates': {
+            'endpoint': 'calendar',
+            'params': {
+                'year': current_football_year
+            }
+        },
+        'media_info': {
+            'endpoint': 'games/media',
+            'params': {
+                'year': current_football_year,
+                'division': 'fbs'
+            } 
+        },
+        'player_stats': {
+            'endpoint': 'stats/player/season',
+            'params': {
+                'year': current_football_year,
+                'seasonType': 'regular'
+            } 
+        },
+        'team_rankings': {
+            'endpoint': 'rankings',
+            'params': {
+                'year': current_football_year,
+                'seasonType': 'regular'
+            }
+        },
+        'conferences': {
+            'endpoint': 'conferences',
+            'params': {}
+        }      
+    }
 
+
+def cache_data(obj: Data, endpoint: str):
+    if not isinstance(obj, Data):
+        raise TypeError(f"Expected instance of Data, but got {type(obj)}")
+    if obj.json_data is None:
+        raise ValueError(f"Object has no JSON data.")
+    if not endpoint:
+        raise TypeError(f"Expected argument endpoint, but got None")
+    if not isinstance(endpoint, str):
+        raise TypeError (f"Endpoint must be of type str")
+    file_name = endpoint.replace("/","_") + ".json"
+    with open(file_name, "w") as outf:
+        outf = json.dump(obj=obj.json_data, fp=outf)
+        
+def load_data(endpoint: str):
+    file_name = endpoint.replace("/", "_") + ".json"
+    try:
+        with open(file_name, "r") as inf:
+            return json.load(inf)
+    except Exception as e:
+        return str(e)
+    
 
 def get_data(obj: Data, endpoint=None, params=None):
     if not isinstance(obj, Data):
-        raise TypeError(f"get_data() expected argument of type Data, but got {type(obj)}")
+        raise TypeError(f"Expected instance of Data, but got {type(obj)}")
     try:
         obj.request(endpoint=endpoint, params=params)
         return f"Success for endpoint: {endpoint}"
@@ -20,82 +97,25 @@ def get_data(obj: Data, endpoint=None, params=None):
         return f" Failure for endpoint: {endpoint}\n{str(e)}"
     
 
-def main():
+def first_data_call():
     not_flat = []
-    fbs_teams = Data()
-    records= Data()
-    game_results= Data()
-    media_info= Data()
-    player_stats= Data()
-    team_rankings= Data()
-    week_dates= Data()
-    conferences= Data()
     
-    data_sources= [{
-        'obj': fbs_teams, 
-        'endpoint': 'teams/fbs',
-        'params': {
-            'year': current_football_year
-        }
-    },
-    {
-        'obj': records, 
-        'endpoint': 'records',
-        'params': {
-            'year': current_football_year
-        }
-    },
-    {
-        'obj': game_results, 
-        'endpoint': 'games',
-        'params': {
-            'year': current_football_year,
-            'division': 'fbs'
-        }
-    },
-    {
-        'obj': week_dates, 
-        'endpoint': 'calendar',
-        'params': {
-            'year': current_football_year
-        }
-    },
-    {
-        'obj': media_info, 
-        'endpoint': 'games/media',
-        'params': {
-            'year': current_football_year,
-            'division': 'fbs'
-        }
-    },
-    {
-        'obj': player_stats, 
-        'endpoint': 'stats/player/season',
-        'params': {
-            'year': current_football_year,
-            'seasonType': 'regular'
-        }
-    },
-    {
-        'obj': team_rankings, 
-        'endpoint': 'rankings',
-        'params': {
-            'year': current_football_year,
-            'seasonType': 'regular'
-        }
-    },
-    {
-        'obj': conferences, 
-        'endpoint': 'conferences',
-        'params': {}
-    }]
+    for source in data_sources.keys():
+        try:
+            obj = Data()
+            obj.request(data_sources[source]['endpoint'], data_sources[source]['params'])
+            cache_data(obj=obj, endpoint=data_sources[source]['endpoint'])
+            if not obj.is_flat(obj.json_data):
+                not_flat.append(data_sources[source]['endpoint'].replace("/", '_') + ".json")
+            print(f"Successful API call for endpoint: {data_sources[source]['endpoint']} \n\n")
+        except Exception as e:
+            print(str(e))
     
-    for source in data_sources:
-        obj= source['obj']
-        get_data(source['obj'], endpoint=source['endpoint'], params=source['params'])
-        if not obj.is_flat(obj.json_data):
-            not_flat.append(obj)
-        print(f"{obj.json_data[0:5]}\n{'-'*30}")
+    with open('not_flat.csv', "w") as outf:
+            writer = csv.writer(outf)
+            for item in not_flat:
+                writer.writerow(item)
+
         
              
 
@@ -132,4 +152,8 @@ df_team_rankings = pd.json_normalize(
 
 
 if __name__ == "__main__":
-    main()
+    '''Make user type second arg for loading data or getting data from API'''
+    if sys.argv[1] == 'new':
+        first_data_call()
+    elif sys.argv[1] == 'load':
+        load_data()
